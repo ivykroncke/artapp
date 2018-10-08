5d2b9293beecb60c72d06bab36b3d5a7baadb74a
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-10-08 19:37:52 -0400

    CRD on second model, artworks - tested via Postman

--- routes/artworksController.js
+++ routes/artworksController.js
@@ -1,9 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router({ mergeParams: true })
+const { User, Artwork } = require('../db/model')
 
-
-router.get('/', function(req, res, next) {
-  res.send('artworks controller hit');
+//Show All Artworks that Belong to a User
+router.get('/', async (req, res) => {
+  const user = await User.findById(req.params.userId)
+  const artworks = user.artworks
+  res.send(artworks)
 });
+
+//Show One Artwork
+router.get('/:id', async (req, res) => {
+  const user = await User.findById(req.params.userId)
+  const artwork = user.artworks.id(req.params.id)
+  res.send(artwork)
+})
+
+//Add Artwork, Post to Database
+router.post('/', (req, res) => {
+  User.findById(req.params.userId)
+  .then(user => {
+    const newArtwork = new Artwork(req.body)
+    console.log(newArtwork)
+    user.artworks.push(newArtwork)
+    return user.save()
+  })
+  .then(user => {
+    res.send(user)
+  })
+})
+
+//Delete Artwork, Remove from Database
+router.delete('/:id', (req, res) => {
+  User.findById(req.params.userId)
+  .then(user => {
+    const artworksNotDeleting = user.artworks.filter(artwork => {
+      return artwork._id != req.params.id
+    })
+    user.artworks = artworksNotDeleting
+    return user.save()
+  })
+  .then(user => {
+    res.send(user)
+  })
+})
 
 module.exports = router;
