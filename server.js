71a9ff9a961edf85d7cbf4cabde2327c21a96f75
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-10-07 22:16:46 -0400

    React App added and concurrently set up.

--- server.js
+++ server.js
@@ -24,13 +24,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/client/build/'));
 
 //ROUTES
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+})
 
 module.exports = app;
