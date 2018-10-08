78fc044d7242fecd1659a577d6a2797b64a4083a
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-10-08 16:52:35 -0400

    Show One and Show All User Routes work

--- routes/usersController.js
+++ routes/usersController.js
@@ -1,7 +1,16 @@
 const router = require('express').Router({ mergeParams: true })
+const { User } = require('../db/model')
 
-router.get('/', function(req, res) {
-  res.send('users controller hit');
-});
+//Show All Users
+router.get('/', async (req, res) => {
+  const users = await User.find()
+  res.send(users)
+})
+
+//Show One
+router.get('/:id', async (req, res) => {
+  const user = await User.findById(req.params.id)
+  res.send(user)
+})
 
 module.exports = router;
