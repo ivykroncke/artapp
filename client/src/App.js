5e5e89a78221ce8d0ef3ee590216f110904aaaf7
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-10-08 21:58:21 -0400

    Set up link to login from home page. Login page has links for all logins to Dashboard page.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Home from './Components/Home';
 import Login from './Components/Login';
+import Dashboard from './Components/Dashboard';
 
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/login' component={Login} />
+          <Route exact path='/users/:userId' component={Dashboard} />
         </Switch>
       </Router>
     )
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        HOME
+        <h1>HOME</h1>
+        <div>
+          <Link to={`/login`}>Login</Link>
+        </div>
       </div>
     )
   }
