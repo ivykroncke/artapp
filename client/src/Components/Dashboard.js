a2144b92629cb28d8cad3600970772fbb7323579
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-10-09 17:50:35 -0400

    Changed the connection to Edit User View to a component so that it is easier connect. May be easier to reveal and conceal this component later than it would be to pass the information along via params...

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import axios from 'axios'
+import EditUserView from './EditUserView';
 
 export default class Dashboard extends Component {
 
   state = {
-    username: ''
+    username: '',
+    userId: ''
   }
 
   getUsername = async () => {
     const userId = this.props.match.params.userId
     const response = await axios.get(`/api/users/${userId}`)
     this.setState({
-      username: response.data.userName
+      username: response.data.userName,
+      userId: response.data.userId,
+      firstName: response.data.firstName,
+      lastName: response.data.lastName
     })
   }
 
@@ -26,7 +30,11 @@ export default class Dashboard extends Component {
       <div>
         <h1>Dashboard</h1>
         <div> Username: {this.state.username}</div>
-          <Link to={`/users/{userId}/edit`}>Edit this user</Link>
+          <EditUserView 
+            username={this.state.username}
+            userId={this.state.userId}
+            firstName={this.state.firstName}
+            lastName={this.state.lastName}/>
       </div>
     )
   }
--- client/src/Components/EditUserView.js
+++ client/src/Components/EditUserView.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class EditUserView extends Component {
-  render() {
-    return (
-      <div>
-        EDIT THAT USER
-      </div>
-    )
-  }
+
+
+    render() {
+        return (
+            <div>
+                <form>
+                    Edit User
+                    <div>
+                        <input name='userName' type='text' placeholder={this.props.username} />
+                    </div>
+                    <div>
+                        <input name='firstName' type='text' placeholder={this.props.firstName} />
+                    </div>
+                    <div>
+                        <input name='lastName' type='text' placeholder={this.props.lastName} />
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
