582e76bb7f14c59b217362767c14bcefb2ab973e
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-10-09 20:27:12 -0400

    State updates in EditUserView when getUser function runs

--- client/src/Components/EditUserView.js
+++ client/src/Components/EditUserView.js
@@ -1,31 +1,45 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 // import axios from 'axios'
 
 export default class EditUserView extends Component {
 
-    updateUser = (event) => {
-        event.preventDefault()
-        const userId = this.props.userId
+    state = {
+        user: {}
+    }
+
+    getUser = async (event) => {
+        const userId = this.props.match.params.userId
+        const response = await axios.get(`/api/users/${userId}`)
+        this.setState({
+            user: response.data
+        })
+
+    }
+
+    componentDidMount = () => {
+        this.getUser()
     }
 
     render() {
+
         return (
             <div>
                 <form>
                     Edit User
                     <div>
-                        <input name='userName' type='text' placeholder={this.props.username} />
+                        <input name='userName' type='text' placeholder={this.state.userName} />
                     </div>
                     <div>
-                        <input name='firstName' type='text' placeholder={this.props.firstName} />
+                        <input name='firstName' type='text' placeholder={this.state.firstName} />
                     </div>
                     <div>
-                        <input name='lastName' type='text' placeholder={this.props.lastName} />
+                        <input name='lastName' type='text' placeholder={this.state.lastName} />
                     </div>
                     <input
                         type='submit'
                         value='Submit Changes'
-                        onClick={this.updateUser}
+                        onClick={this.getUser}
                     />
                 </form>
             </div>
